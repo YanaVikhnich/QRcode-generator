ee2d67ec9cc4691ce7df233d4942262c052e6c5f
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2026-01-20 15:14:35 +0200

    Added error message and was com

--- js/qrcode-generator.js
+++ js/qrcode-generator.js
@@ -2,63 +2,84 @@ const qrcodeGenerateBtn = document.querySelector(".qr-generate-button");
 const qrCodeContainer = document.querySelector(".QRcode-container");
 const downloadBtn = document.querySelector(".download-button");
 const shareBtn = document.querySelector(".share-button");
-const URL = document.querySelector('.input-control');
+const URLfield = document.querySelector(".input-control");
 const urlContainer = document.querySelector(".URL-container");
 const QRcode = document.querySelector(".QR");
+const inputBtnContainer = document.querySelector(".input-button-container");
 
-let qrcode
+let qrcode;
 function generateQRCode(text) {
-    qrcode = new QRCode(QRcode, {
-      text: text,
-      width: 164,
-      height: 164,
-      colorDark: "#030616",
-      colorLight: "#fff",
-      correctLevel: QRCode.CorrectLevel.H,
-    });
+  qrcode = new QRCode(QRcode, {
+    text: text,
+    width: 164,
+    height: 164,
+    colorDark: "#030616",
+    colorLight: "#fff",
+    correctLevel: QRCode.CorrectLevel.H,
+  });
 }
+URLfield.addEventListener("input", () => {
+    clearError();
+});
 
 
 qrcodeGenerateBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    let qrContent = URL.value;
+  e.preventDefault();
+    
+  let qrContent = URLfield.value;
     if (qrContent != "") {
-        generateQRCode(qrContent);
-    }
+      
+    generateQRCode(qrContent);
+    
     urlContainer.classList.toggle("hidden");
     qrCodeContainer.classList.toggle("hidden");
-
- })
+  } else {
+    showError();
+  }
+});
 
 downloadBtn.addEventListener("click", () => {
-    let img = QRcode.querySelector("img");
-    let imgSrc = img.src;
-    let a = document.createElement("a");
-    a.href = imgSrc;
-    a.download = "qrcode.png";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+  let img = QRcode.querySelector("img");
+  let imgSrc = img.src;
+  let a = document.createElement("a");
+  a.href = imgSrc;
+  a.download = "qrcode.png";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 });
 
 shareBtn.addEventListener("click", async () => {
-    let img = QRcode.querySelector("img");
-    let imgSRC = img.src;
-    try {
-        const response = await fetch(imgSRC);
-        const blob = await response.blob();
-        const filesArray = [new File([blob], "qrcode.png", { type: blob.type })];
-        if (navigator.canShare && navigator.canShare(filesArray)) {
-            await navigator.share({
-                title: "QR Code",
-                files: filesArray,
-            });
-        } else {
-            window.open(imgSRC);
-            console.error("Sharing not supported or not enabled");
-        }
-    } catch (error) {
-        console.error("Error sharing QR code:", error);
+  let img = QRcode.querySelector("img");
+  let imgSRC = img.src;
+  try {
+    const response = await fetch(imgSRC);
+    const blob = await response.blob();
+    const filesArray = [new File([blob], "qrcode.png", { type: blob.type })];
+    if (navigator.canShare && navigator.canShare(filesArray)) {
+      await navigator.share({
+        title: "QR Code",
+        files: filesArray,
+      });
+    } else {
+      window.open(imgSRC);
+      console.error("Sharing not supported or not enabled");
     }
-})
+  } catch (error) {
+    console.error("Error sharing QR code:", error);
+  }
+});
+
+function clearError() {
+    errorMes = inputBtnContainer.nextElementSibling;
+    errorMes.classList.remove("error-message");
+      errorMes.textContent = "";
+      errorMes.remove();
+}
+function showError() {
+    errorSpan = document.createElement("span");
+  inputBtnContainer.after(errorSpan);
+  errorSpan = inputBtnContainer.nextElementSibling;
+  errorSpan.classList.add("error-message");
+  errorSpan.textContent = `You need to enter the url`;
+}
